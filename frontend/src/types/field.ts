28d0966a0ef452966b6a2e334d1b1907e30aa742
sitscholl <tscholl.simon@gmail.com>
2026-03-13 09:27:29 +0100

    move FieldContainer to types

--- frontend/src/types/field.ts
+++ frontend/src/types/field.ts
@@ -1,10 +1,10 @@
-export type FieldContainer = {
+export type FieldSummary = {
   id: number
   name: string
   reference_station: string
   soil_type: string
   humus_pct: number
-  area_ha: number
+  area_ha: number | null
   root_depth_cm: number
   p_allowable: number
 }
