66eeb36b9deff3ad84165cad1be473acf74ddd7a
sitscholl <tscholl.simon@gmail.com>
2026-03-13 08:13:24 +0100

    add uvicorn startup code

--- backend/main.py
+++ backend/main.py
@@ -1,33 +1,11 @@
-#Example for base app classes structure: https://github.com/kthorp/pyfao56/blob/main/tests/test01/cottondry2013.py
+import uvicorn
+
 import logging
-import logging.config
-from datetime import datetime
 
-from src.config import load_config
-from src.database.db import IrrigDB
-from src.workflow import WaterBalanceWorkflow
+from src.api import app
 
+# Configure logging
+#log_handler = LogHandler.from_file("config.logging.yaml")
+#log_handler.start_logger()
 
-logger = logging.getLogger(__name__)
-
-if __name__ == "__main__":
-    
-    config = load_config('config/config.yaml')
-    logging.config.dictConfig(config['logging'])
-
-    logger.info("#"*50)
-    logger.info('Starting water balance calculation')
-    logger.info("#"*50)
-    
-    db = IrrigDB(**config.get('database', {}))
-
-    workflow = WaterBalanceWorkflow(config, db)
-    
-    try:
-        workflow.run()
-    except Exception as e:
-        logger.error(f"Error running water balance workflow: {e}", exc_info = True)
-
-    logger.info("#"*50)
-    logger.info('Water balance calculation finished')
-    logger.info("#"*50)
+uvicorn.run(app, host="0.0.0.0", port=8000, log_level=logging.DEBUG)
